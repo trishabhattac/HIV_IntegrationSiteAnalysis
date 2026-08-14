22f71e0923a67e0a38f7757316961f267927cb03
Trisha Bhattacharyya <trishabhattacharyya@Trishas-MacBook-Air.local>
2026-08-14 12:07:44 +0100

    python scripts for initial sannity check of 100 datasets

--- scripts/trial_script.py
+++
@@ -1,8 +0,0 @@
-import mappy as mp
-
-fastq_path = "files/output_files/filtered_reads/77_3_filtered.fastq"
-
-for read_id, seq, qual in mp.fastx_read(fastq_path):
-    print(f"Read ID: {read_id}")
-    print(f"First 200 bp:\n{seq[:200]}")
-    break
